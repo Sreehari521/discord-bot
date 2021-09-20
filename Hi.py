c2a791eb25d5cf5dc53e95d4b8524e781f3aaee5
Sreehari521 <82920449+Sreehari521@users.noreply.github.com>
2021-09-20 17:00:38 +0530

    Add files via upload

---
+++ Hi.py
@@ -0,0 +1,37 @@
+import discord
+import random
+
+TOKEN = "ODg5MzU2ODY5MzY0MjQwMzg1.YUgEIQ.tHk_iIo6oVckCQKJzRMvtDWsk6c"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have loged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
+
+    if message.author == client:
+        return
+    
+    if message.channel.name == "general":
+        if user_message.lower() == "hello":
+            await message.channel.send(f'Hello {username}!')
+            return
+        elif user_message.lower() == "bye":
+            await message.channel.send(f'See you later {username}!')
+            return
+        elif user_message.lower() == "!random":
+            response = f'This is your random number: {random.randrange(10000)}'
+            await message.channel.send(response)
+            return
+        elif user_message.lower() == "!important":
+            await message.channel.send("@everyone Come here it is important")
+            return
+
+client.run(TOKEN)
